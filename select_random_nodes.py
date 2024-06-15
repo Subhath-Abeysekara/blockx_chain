a39dde8aff0ee8d8988f1b202a3e1d1a20803e62
jerom <jeromroshane911@gmail.com>
2024-06-15 19:09:39 +0530

    limit publick keys

--- select_random_nodes.py
+++ select_random_nodes.py
@@ -5,9 +5,13 @@ import random
 def select_nodes(public_key):
     dir_list = os.listdir("nodes")
     print(dir_list)
-    dir_list.remove(public_key)
+    try:
+        dir_list.remove(public_key)
+    except:
+        print("directory not available")
     print(dir_list)
-    num_elements_to_pick = random.randint(2, len(dir_list))
+    max_nodes = len(dir_list) if len(dir_list)<8 else 8
+    num_elements_to_pick = random.randint(2, max_nodes)
     random_elements = random.sample(dir_list, num_elements_to_pick)
     print(num_elements_to_pick , random_elements)
     return {
